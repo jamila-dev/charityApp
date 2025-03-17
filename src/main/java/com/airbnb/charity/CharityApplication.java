b43457b2b10d60a518e022d77b0d187760d7b177
jamila-dev <jamila_dev>
2025-03-17 11:59:15 +0000

    test commit

--- src/main/java/com/airbnb/charity/CharityApplication.java
+++ src/main/java/com/airbnb/charity/CharityApplication.java
@@ -1,13 +1,18 @@
 package com.airbnb.charity;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CharityApplication {
+public class CharityApplication  implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
